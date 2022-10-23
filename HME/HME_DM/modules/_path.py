7f2bdeecce5593962a057f061ca934edc86b9c83
MTamon <mtamon.0044@gmail.com>
2022-10-24 06:22:48 +0900

    [add] _path.py

--- HME/HME_DM/modules/_path.py
+++ HME/HME_DM/modules/_path.py
@@ -5,3 +5,8 @@ import sys
 
 MODULE_PATH = "/".join(os.path.dirname(__file__).split(os.sep))
 sys.path.append(MODULE_PATH)
+
+mem_list = os.listdir(MODULE_PATH)
+for m in mem_list:
+    if os.path.isdir(m):
+        sys.path.append("/".join([MODULE_PATH, m]))
