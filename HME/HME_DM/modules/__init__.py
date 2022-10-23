980e64ca99ef1df3edb2f1472b27a613dfc4222d
MTamon <mtamon.0044@gmail.com>
2022-10-24 06:05:58 +0900

    [add] _path.py

--- HME/HME_DM/modules/__init__.py
+++ HME/HME_DM/modules/__init__.py
@@ -1,5 +1,6 @@
 """Modules Import"""
 
+import _path
 from .DataFileCollector.data_collect import Collector
 from .DataFileCollector.material import Directory, Condition
 from .FaceMotionDetection.argments import get_args, add_args
