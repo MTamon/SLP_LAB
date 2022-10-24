b7307b58d4e793330ba1cb8062e522de250c25f5
MTamon <mtamon.0044@gmail.com>
2022-10-24 13:57:22 +0900

    [add] mp4 new condition for CEJC

--- HME/HME_DM/process.py
+++ HME/HME_DM/process.py
@@ -2,10 +2,36 @@
 
 from argparse import Namespace
 from logging import Logger
+import os
 from modules import Collector, Condition
 from modules import Extraction
 from utils import get_args, get_extraction_args
 from logger_gen import set_logger
+
+
+def cejc_condition(path: str) -> bool:
+    """For CEJC dataset's condition"""
+
+    dir_path = os.path.dirname(path)
+    fil_name = os.path.basename(path)
+    member_list = os.listdir(dir_path)
+    _m_l = []
+
+    if not "_MIX" in fil_name:
+        return True
+
+    all_mix_flg = True
+    for member in member_list:
+        if not os.path.isfile(os.path.join(dir_path, member)):
+            continue
+        if member[-4:] != ".mp4":
+            continue
+
+        _m_l.append(member)
+        if not "_MIX" in member:
+            all_mix_flg = False
+
+    return all_mix_flg
 
 
 class MakeData:
@@ -18,10 +44,13 @@ class MakeData:
         self.target = args.target
         self.output = args.output
 
-        self.conditions = (
-            Condition().specify_extention("mp4"),
-            Condition().specify_extention("wav"),
-        )
+        condition1 = Condition().specify_extention(["mp4"])
+        condition1.add_exclude_filename(["_HD", "_SK"])
+        condition1.add_condition_func(cejc_condition)
+
+        condition2 = Condition().specify_extention(["wav"])
+
+        self.conditions = (condition1, condition2)
 
         self.collector = Collector(self.conditions[0], self.target)
         self.extractor = Extraction(logger, args)
