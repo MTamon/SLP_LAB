4839c95143f15f725c1ad1b888ea469692f6c84c
MTamon <mtamon.0044@gmail.com>
2022-12-10 03:18:43 +0900

    [fix] bug

--- HME/MFCC_DM/mfcc_building.py
+++ HME/MFCC_DM/mfcc_building.py
@@ -63,7 +63,7 @@ class Mfcc_Segment:
         self.segment_min_size = args.segment_min_size
 
         self.sep_data = args.sep_data
-        self.feature = args.used_feature
+        self.feature = args.use_feature
 
         self.video_fps = args.video_fps
 
@@ -97,12 +97,13 @@ class Mfcc_Segment:
             dither=args.dither,
         )
 
+        self.output = "/".join(re.split(r"[\\]", self.output))
         if not os.path.isdir(self.output):
             os.mkdir(self.output)
-        self.temp_path = "/".join(re.split(r"[\\]", self.output).append("tmp"))
+        self.temp_path = "/".join(self.output + "/tmp")
         if not os.path.isdir(self.temp_path):
             os.mkdir(self.temp_path)
-        self.seg_path = "/".join(re.split(r"[\\]", self.output).append("data"))
+        self.seg_path = "/".join(self.output + "/data")
         if not os.path.isdir(self.seg_path):
             os.mkdir(self.seg_path)
 
