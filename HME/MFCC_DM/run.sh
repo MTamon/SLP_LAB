b0fdc4e20c2fe8605f0a33b53da424affd7ac6c7
MTamon <mtamon.0044@gmail.com>
2022-12-10 19:50:28 +0900

    [add] run.sh

---
+++ HME/MFCC_DM/run.sh
@@ -0,0 +1,18 @@
+python mfcc_building.py \
+--target C:\\Users\\Mikawa\\Desktop\\Life\\GitHub\\SLP_LAB\\HME\\data\\test\\post_out\\post \
+--output C:\\Users\\Mikawa\\Desktop\\Life\\GitHub\\SLP_LAB\\HME\\data\\test\\segments \
+--log log/segment.log \
+--segment-size 15 \
+--segment-stride 5 \
+--segment-min-size 10 \
+--sep_data --use-feature fbank \
+--video-fps 29.97 \
+--sample-frequency 16000 \
+--frame-length 32 \
+--frame-shift 13 \
+--num-mel-bins 80 \
+--num-ceps 13 \
+--low-frequency 20 \
+--high-frequency 8000 \
+--dither 1.0 \
+--proc-num 7 \
