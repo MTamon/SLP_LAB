5f4ce7f58b912b1d633529c557291154bd729f61
MTamon <mtamon.0044@gmail.com>
2022-12-13 08:16:24 +0900

    [add] branch

--- HME/MODEL/hme_trainer.py
+++ HME/MODEL/hme_trainer.py
@@ -49,44 +49,43 @@ class HmeTrainer(Trainer):
         trg_angl, _ = self.padding(batch["target"]["angl"])
         trg_cent, _ = self.padding(batch["target"]["cent"])
 
-        with torch.cuda.amp.autocast():
-            if self.mode == "train":
-                model_input = [src_angl, src_cent, src_trgt, src_othr]
-                (pred_angl, pred_cent), _ = self.net(model_input)
-            else:
-                pred_angl = torch.zeros_like(src_angl[:, 0:1, :])
-                pred_cent = torch.zeros_like(src_cent[:, 0:1, :])
-                h, c = None, None
-                _angl = torch.unsqueeze(src_angl[:, 0, :], dim=1)
-                _cent = torch.unsqueeze(src_cent[:, 0, :], dim=1)
+        if self.mode == "train":
+            model_input = [src_angl, src_cent, src_trgt, src_othr]
+            (pred_angl, pred_cent), _ = self.net(model_input)
+        else:
+            pred_angl = torch.zeros_like(src_angl[:, 0:1, :])
+            pred_cent = torch.zeros_like(src_cent[:, 0:1, :])
+            h, c = None, None
+            _angl = torch.unsqueeze(src_angl[:, 0, :], dim=1)
+            _cent = torch.unsqueeze(src_cent[:, 0, :], dim=1)
 
-                for _seq in range(src_angl.shape[1]):
-                    _trgt = torch.unsqueeze(src_trgt[:, _seq, :], dim=1)
-                    _othr = torch.unsqueeze(src_othr[:, _seq, :], dim=1)
+            for _seq in range(src_angl.shape[1]):
+                _trgt = torch.unsqueeze(src_trgt[:, _seq, :], dim=1)
+                _othr = torch.unsqueeze(src_othr[:, _seq, :], dim=1)
 
-                    model_input = [_angl, _cent, _trgt, _othr]
-                    (_p_angl, _p_cent), (h, c) = self.net(model_input, h, c)
+                model_input = [_angl, _cent, _trgt, _othr]
+                (_p_angl, _p_cent), (h, c) = self.net(model_input, h, c)
 
-                    pred_angl = torch.cat((pred_angl, _p_angl), axis=1)
-                    pred_cent = torch.cat((pred_cent, _p_cent), axis=1)
+                pred_angl = torch.cat((pred_angl, _p_angl), axis=1)
+                pred_cent = torch.cat((pred_cent, _p_cent), axis=1)
 
-                    _angl, _cent = _p_angl, _p_cent
+                _angl, _cent = _p_angl, _p_cent
 
-                pred_angl = pred_angl[:, 1:, :]
-                pred_cent = pred_cent[:, 1:, :]
+            pred_angl = pred_angl[:, 1:, :]
+            pred_cent = pred_cent[:, 1:, :]
 
-            pred_angl *= mask_a
-            trg_angl *= mask_a
-            pred_cent *= mask_c
-            trg_cent *= mask_c
+        pred_angl *= mask_a
+        trg_angl *= mask_a
+        pred_cent *= mask_c
+        trg_cent *= mask_c
 
-            div = torch.sum(mask_a) * 2
+        div = torch.sum(mask_a) * 2
 
-            loss_angl = torch.sum((pred_angl - trg_angl) ** 2) / div
-            loss_cent = torch.sum((pred_cent - trg_cent) ** 2) / div
+        loss_angl = torch.sum((pred_angl - trg_angl) ** 2) / div
+        loss_cent = torch.sum((pred_cent - trg_cent) ** 2) / div
 
-            loss = loss_angl + loss_cent
-            self.learn(loss)
+        loss = loss_angl + loss_cent
+        self.learn(loss)
 
         acc = torch.sum(abs(pred_angl - trg_angl) + abs(pred_cent - trg_cent)) / div
 
