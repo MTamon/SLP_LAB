f4a1479d74ae2ceb727a8a442232f5435f7f79b7
MTamon <mtamon.0044@gmail.com>
2022-12-13 21:25:17 +0900

    [fix] bug

---
+++ HME/MODEL/runhj.sh
@@ -0,0 +1,23 @@
+python main.py \
+--datasite /datasets/mikawa/segments/all \
+--log log/train.log \
+--train-result-path transition/train.csv \
+--valid-result-path transition/valid.csv \
+--ac-feature-size 80 \
+--ac-feature-width 32 \
+--lstm-dim 16 \
+--lstm-input-dim 32 \
+--lstm-output-dim 16 \
+--num-layer 2 \
+--ac-linear-dim 8 \
+--batch-size 128 \
+--valid-rate 0.3 \
+--truncate-excess \
+--do-shuffle \
+--epoch 20 \
+--model-save-path model/_model.pth \
+--lr 1e-4 \
+--beta1 0.9 \
+--beta2 0.98 \
+--eps 1e-8 \
+--weight-decay 1e-6
