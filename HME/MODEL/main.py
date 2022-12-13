8bbe204372367e7cea7342fffb3c971aa199df1a
MTamon <mtamon.0044@gmail.com>
2022-12-13 23:33:34 +0900

    [fix] display phase(train, valid) mean loss and acc @ main.py

--- HME/MODEL/main.py
+++ HME/MODEL/main.py
@@ -56,27 +56,42 @@ v_f = open(valid_fpath, mode="w", encoding="utf-8")
 v_f.write(f"loss, acc, {valid_length}\n")
 v_f.close()
 
+
+def process(_mode):
+    phase_loss = []
+    phase_acc = []
+
+    with tqdm(trainer, desc=_mode) as prog:
+        for loss, acc in prog:
+
+            if _mode == "train":
+                with open(train_fpath, mode="a", encoding="utf-8") as f:
+                    f.write(f"{loss},{acc}\n")
+            else:
+                with open(valid_fpath, mode="a", encoding="utf-8") as f:
+                    f.write(f"{loss},{acc}\n")
+
+            _loss = round(float(loss.to("cpu")), 2)
+            _acc = round(float(acc.to("cpu")), 2)
+            prog.postfix = f"L:{_loss}, A:{_acc}"
+
+            phase_loss.append(float(loss))
+            phase_acc.append(float(acc))
+
+    phase_loss = sum(phase_loss) / len(phase_loss)
+    phase_acc = sum(phase_acc) / len(phase_acc)
+
+    return phase_loss, phase_acc
+
+
 for current_epoch in range(args.epoch):
-    epoch_loss = []
-    epoch_acc = []
     logger.info(" Epoch >>> %s / %s", (current_epoch + 1), args.epoch)
 
     for mode in ["train", "valid"]:
         trainer.set_mode(mode)
 
-        with tqdm(trainer, desc=mode) as prog:
-            for loss, acc in prog:
-
-                if mode == "train":
-                    with open(train_fpath, mode="a", encoding="utf-8") as f:
-                        f.write(f"{loss},{acc}\n")
-                else:
-                    with open(valid_fpath, mode="a", encoding="utf-8") as f:
-                        f.write(f"{loss},{acc}\n")
-
-                _loss = round(float(loss.to("cpu")), 2)
-                _acc = round(float(acc.to("cpu")), 2)
-                prog.postfix = f"L:{_loss}, A:{_acc}"
+        _loss, _acc = process(mode)
+        logger.info(" Result |[ Loss : %s, Acc : %s ]|", _loss, _acc)
 
     epo_inf = f"0{current_epoch}" if current_epoch < 10 else str(current_epoch)
     path = ".".join(args.model_save_path.split(".")[:-1]) + f"E{epo_inf}.pth"
