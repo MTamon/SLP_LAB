561d0fa641736b953a646f6b59d33473c6b24f27
MTamon <mtamon.0044@gmail.com>
2022-12-21 21:23:09 +0900

    [add] joblib

--- HME/MODEL/ohta_dataloader.py
+++ HME/MODEL/ohta_dataloader.py
@@ -1,9 +1,10 @@
 """For Simple-Model Dataloader"""
 
 from __future__ import annotations
-from typing import List, Dict
+from typing import List, Dict, Tuple
 import torch
 import joblib
+import pickle
 
 from src import override
 from src import Dataloader
@@ -23,6 +24,7 @@ class OhtaDataloader(Dataloader):
         super().__init__(
             dataset, batch_size, truncate_excess, do_shuffle, *args, **kwargs
         )
+        self.dataset = dataset
         self.args = args
         self.kwargs = kwargs
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -31,11 +33,12 @@ class OhtaDataloader(Dataloader):
     def collect_batch(
         self, batch_idx: List[int]
     ) -> Dict[str, Dict[str, List[torch.Tensor]]]:
-        record = self.dataset[batch_idx[0]]
+        record = OhtaDataloader.get_data(*self._get_args(0))
         _batch = record
 
         result = joblib.Parallel(n_jobs=-1)(
-            joblib.delayed(self.thread)(i) for i in batch_idx[1:]
+            joblib.delayed(OhtaDataloader.get_data)(*self._get_args(i))
+            for i in batch_idx[1:]
         )
 
         for record in result:
@@ -93,8 +96,102 @@ class OhtaDataloader(Dataloader):
 
         return batch
 
-    def thread(self, idx):
-        return self.dataset[idx]
+    @staticmethod
+    def get_data(
+        datasite: str,
+        file_pointer: Tuple[str, int],
+        acostic_frame_width: int,
+        physics_frame_width: int,
+        acostic_dim: int,
+    ) -> Tuple[List[torch.Tensor], List[torch.Tensor]]:
+        segment_path = datasite + "/" + file_pointer[0]
+        with open(segment_path, "rb") as seg:
+            segment = pickle.load(seg)
+
+        vfps = segment["vfps"]
+        ffps = segment["ffps"]
+
+        common = {
+            "device": torch.device("cpu"),
+            "dtype": torch.float32,
+            "requires_grad": True,
+        }
+
+        i = file_pointer[1]
+
+        fframe = int(i / vfps * ffps)
+        ac_prev_idx = fframe + 1 - acostic_frame_width
+        ph_prev_idx = i + 1 - physics_frame_width
+
+        trgt_window = torch.tensor(segment["trgt"][ac_prev_idx : fframe + 1], **common)
+        othr_window = torch.tensor(segment["othr"][ac_prev_idx : fframe + 1], **common)
+        trgt_lpower = torch.tensor(segment["tlgp"][ac_prev_idx : fframe + 1], **common)
+        othr_lpower = torch.tensor(segment["olgp"][ac_prev_idx : fframe + 1], **common)
+
+        assert trgt_window.shape[0] == acostic_frame_width
+        assert othr_window.shape[0] == acostic_frame_width
+        assert trgt_window.shape[1] == acostic_dim
+        assert othr_window.shape[1] == acostic_dim
+        assert len(trgt_lpower.shape) == 1, f"FILE : {file_pointer[0]}"
+        assert len(othr_lpower.shape) == 1, f"FILE : {file_pointer[0]}"
+
+        _trgt_window = torch.tensor(segment["trgt"][ac_prev_idx - 1 : fframe], **common)
+        _othr_window = torch.tensor(segment["othr"][ac_prev_idx - 1 : fframe], **common)
+        dtrgt_window = OhtaDataloader.clone_detach(trgt_window - _trgt_window)
+        dothr_window = OhtaDataloader.clone_detach(othr_window - _othr_window)
+
+        _trgt_lpower = torch.tensor(segment["tlgp"][ac_prev_idx - 1 : fframe], **common)
+        _othr_lpower = torch.tensor(segment["olgp"][ac_prev_idx - 1 : fframe], **common)
+        dtrgt_lpower = OhtaDataloader.clone_detach(trgt_lpower - _trgt_lpower)
+        dothr_lpower = OhtaDataloader.clone_detach(othr_lpower - _othr_lpower)
+
+        cent_window = torch.tensor(segment["cent"][ph_prev_idx : i + 1], **common)
+        angl_window = torch.tensor(segment["angl"][ph_prev_idx : i + 1], **common)
+
+        _cent_window = torch.tensor(segment["cent"][ph_prev_idx - 1 : i], **common)
+        _angl_window = torch.tensor(segment["angl"][ph_prev_idx - 1 : i], **common)
+        dcent_window = OhtaDataloader.clone_detach(cent_window - _cent_window)
+        dangl_window = OhtaDataloader.clone_detach(angl_window - _angl_window)
+
+        ans_angl = segment["angl"][i + 1] - segment["angl"][i]
+        ans_cent = segment["cent"][i + 1] - segment["cent"][i]
+
+        one_set = (
+            [
+                [angl_window],
+                [dangl_window],
+                [cent_window],
+                [dcent_window],
+                [trgt_window],
+                [dtrgt_window],
+                [othr_window],
+                [dothr_window],
+                [trgt_lpower],
+                [dtrgt_lpower],
+                [othr_lpower],
+                [dothr_lpower],
+            ],
+            [
+                [torch.tensor(ans_angl, **common)],
+                [torch.tensor(ans_cent, **common)],
+            ],
+            file_pointer[0],
+        )
+
+        return one_set
+
+    @staticmethod
+    def clone_detach(tensor: torch.Tensor):
+        return tensor.clone().detach().requires_grad_(True)
+
+    def _get_args(self, idx):
+        return (
+            self.dataset.datasite,
+            self.dataset[idx],
+            self.dataset.acostic_frame_width,
+            self.dataset.physic_frame_width,
+            self.dataset.acostic_dim,
+        )
 
     @override(Dataloader)
     def copy(self) -> OhtaDataloader:
