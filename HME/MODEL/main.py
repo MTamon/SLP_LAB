5e1115528cc2bd90b7e8470f9ee118f68c4a4dff
MTamon <mtamon.0044@gmail.com>
2022-12-25 23:29:34 +0900

    [fix]

--- HME/MODEL/main.py
+++ HME/MODEL/main.py
@@ -94,15 +94,14 @@ def process(_mode):
     return phase_loss, phase_acc
 
 
-# logger.info(" Init Valid-Mode >>> ")
-# _loss, _acc = process("valid")
-# logger.info(" Result |[ Loss : %s, Acc : %s ]|", round(_loss, 2), round(_acc, 2))
+logger.info(" Init Valid-Mode >>> ")
+_loss, _acc = process("valid")
+logger.info(" Result |[ Loss : %s, Acc : %s ]|", round(_loss, 2), round(_acc, 2))
 
 for current_epoch in range(args.epoch):
     logger.info(" Epoch >>> %s / %s", (current_epoch + 1), args.epoch)
 
-    # for mode in ["train", "valid"]:
-    for mode in ["train"]:
+    for mode in ["train", "valid"]:
         _loss, _acc = process(mode)
         logger.info(" Result |[ Loss : %s, Acc : %s ]|", _loss, _acc)
 
